11b1bbb6b3e8c6cb5522543259c81cc0af9f8e27
Hoàng Sang <phsang49@gmail.com>
2021-09-29 12:57:08 +0700

    First commit

---
+++ Main.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+from FTPClient import Ui_MainWindow
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+        self.uic.btnOk.clicked.connect(self.loader)
+        self.uic.btnCancel.clicked.connect(self.main_win.close)
+
+    def loader(self):
+        self.uic.textEdit.setText("I'm not Iron man")
+
+    def show(self):
+        self.main_win.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
