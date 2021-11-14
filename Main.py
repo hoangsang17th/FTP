76cdb2e13849723258e828a3ec3bad36ee4cac18
Hoàng Sang <phsang49@gmail.com>
2021-11-14 21:25:07 +0700

    Hôm nay tôi đã làm được thêm tính năng hiển thị menu cho server và local, Hiển thị dữ liệu của server truyền về, 1 số tính năng nhỏ bên trong menu của Local

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QIcon
 from PyQt5.QtWidgets import QApplication,QMainWindow, QFileSystemModel, QTreeWidgetItem, QWidget, QMenu
 from PyQt5 import QtCore
 from FTPClient import Ui_MainWindow
@@ -10,6 +10,8 @@ import webbrowser
 from MySQLConnection import updateAccount, selectAccount
 from Dialogs.FTPClientSettings import Ui_settingsWindow
 from datetime import datetime
+app_icon_path = os.path.join(os.path.dirname(__file__), 'Images')
+qIcon = lambda name: QIcon(os.path.join(app_icon_path, name))
 class MainWindow():
     def __init__(self):
         self.main_win = QMainWindow()
@@ -17,6 +19,7 @@ class MainWindow():
         self.uic.setupUi(self.main_win)
         self.uic.model = QFileSystemModel()
         self.uic.inputDirPathLocal.setText("C://")
+        self.pathLocal = "C://"
         dirPathLocal = self.uic.inputDirPathLocal.text()
         self.uic.model.setRootPath(r""+dirPathLocal)
         self.uic.loadDirPathLocal.clicked.connect(self.loadDirPathLocal)
@@ -29,13 +32,16 @@ class MainWindow():
         self.uic.loadDirPathServer.clicked.connect(self.loadDirPathServer)
         self.uic.actionExit.triggered.connect(self.main_win.close)
         self.uic.actionSettings.triggered.connect(self.openSettings)
-        self.uic.folderServer.customContextMenuRequested.connect(self.show_context_menu)
+        self.uic.folderServer.setAlternatingRowColors(True)
+        self.pathServer = "Not connected to any server"
         dataMySQL = selectAccount()
         self.uic.inputHost.setText(dataMySQL[1])
         self.uic.inputUser.setText(dataMySQL[2])
         self.uic.inputPass.setText(dataMySQL[3])
         self.uic.inputPort.setText(dataMySQL[4])
         self.ftp = FTP()
+        self.uic.folderLocal.customContextMenuRequested.connect(self.contextMenuLocal)
+        # print(self.convert_bytes(1380408))
         # force UTF-8 encoding
         self.ftp.encoding = "utf-8"
         # self.uic.actionAbout.triggered.connect(self.open_url("https://www.facebook.com/"))
@@ -68,6 +74,7 @@ class MainWindow():
                         self.ftp.quit()
                         self.printStatus("DisConnect Success.")
                         self.uic.btnConnect.setText("QuickConnect")
+                        self.uic.folderServer.clear()
                         self.disabledComponents()
                     except:
                         self.printError("DisConnect Failed.")
@@ -100,7 +107,7 @@ class MainWindow():
                         # except ValueError:
                         #     print(ValueError)
                         # print(ftp.nlst())
-                        # print(self.ftp.list())
+                        # print(self.ftp.dir())
                     except:
                         self.printError("Incorret Host/User/Pass or Port!")
         except:
@@ -108,30 +115,116 @@ class MainWindow():
         # self.printStatus("User authentication successful.")
 
     #############################################################
-    # @QtCore.pyqtSlot('QPoint')
-    def show_context_menu(self, point):
-        """Show the context menu."""
-        index = self.indexAt(point)
-        if index.isValid():
-            self.testPrint()
-        else:
-            item = None
-        self._menu = QMenu(self)
-        actions = self._get_menu_actions(item)
-        for (name, handler) in actions:
-            if name is None and handler is None:
-                self._menu.addSeparator()
-            else:
-                assert name is not None
-                assert handler is not None
-                action = self._menu.addAction(name)
-                action.triggered.connect(handler)
-        if actions:
-            self._menu.popup(self.viewport().mapToGlobal(point))
+    def downloadToRemoteFileList(self):
+        """
+        download file and directory list from FTP Server
+        """
+        self.remoteWordList = []
+        self.remoteDir      = {}
+        self.ftp.dir('.', self.addItemToRemoteFileList)
+        # self.Remote_completerModel.setStringList(self.remoteWordList)
 
+    def addItemToRemoteFileList(self, content):
+        ftype, size, date, filename = self.parseFileInfo(content)
+        if content.startswith('d'):
+            icon = qIcon('folder.png')
+            pathname = os.path.join(self.pathServer, filename)
+            pathname = pathname.replace('\\', '/')
+            self.remoteDir[ pathname] = True
+            self.remoteWordList.append(filename)
+        else:
+            icon = qIcon('file.png')
+        item = QTreeWidgetItem()
+        item.setIcon(0, icon)
+        for n, i in enumerate((filename, size, ftype, date)):
+            if n == 2:
+                if i == "drwxr-xr-x":
+                    i = "Folder"
+                elif i == "---x--x--x":
+                    i = "Application"
+                else:
+                    i = "File"
+
+            if n == 1:
+                if i != "0":
+                    num = int(i)
+                    for x in ['Byte', 'KB', 'MB', 'GB', 'TB']:
+                        if num < 1024:
+                            num = f"{round(num, 2)}{x}"
+                            break
+                        num /= 1024
+                    i = str(num)
+                    # i = self.convert_bytes(1380408)
+            item.setText(n, i)
+            # print(n, i)
+
+        self.uic.folderServer.addTopLevelItem(item)
+        if not self.uic.folderServer.currentItem():
+            self.uic.folderServer.setCurrentItem(self.uic.folderServer.topLevelItem(0))
+            self.uic.folderServer.setEnabled(True)
+        for i in range(4):
+            self.uic.folderServer.resizeColumnToContents(i)
+        # self.uic.folderServer.resizeColumnToContents(1)
+        # self.uic.folderServer.resizeColumnToContents(2)
+        # self.uic.folderServer.resizeColumnToContents(3)
+
+    def parseFileInfo(self, file):
+        """
+        parse files information "drwxr-xr-x", "2", "root", "wheel", "1024", "Nov 17 1993", "lib"
+        """
+        # FileMode, FilesNumber, User, Group, Size, Date, Filename
+        item = [f for f in file.split(' ') if f != '']
+        
+        ftype, size, date, filename = (item[0],  item[4], ' '.join(item[5:8]), ' '.join(item[8:]))
+        # print(ftype, size, date, filename)
+        return (ftype, size, date, filename)
+
+
+    # Hàm hiển thị menu các chức năng để tương tác với folder client
+    def contextMenuLocal(self, point):
+        item = self.uic.folderLocal.selectedIndexes()
+        menu = QMenu(self.uic.folderLocal)
+        upload = menu.addAction("Upload")
+        upload.setEnabled(False)
+        open = menu.addAction("Open")
+        menu.addSeparator()
+        mkdir = menu.addAction("Create directory")
+        newfile = menu.addAction("Create new file")
+        refresh = menu.addAction("Refresh")
+        menu.addSeparator()
+        remove = menu.addAction("Delete")
+        rename = menu.addAction("Rename")
+        
+        action = menu.exec_(self.uic.folderLocal.mapToGlobal(point))
+        if action == refresh:
+            self.testPrint()
+        if action == open:
+            # self.uic.folderLocal.itemClicked.setText()
+            try:
+                os.startfile(self.pathLocal)
+            except:
+                error = "The system cannot find the drive specified: '"+ self.pathLocal+ "'"
+                self.printError(error)
     # Hàm hiển thị menu các chức năng để tương tác với folder server
-    def contextMenuServer(self):
-        print("Ok")
+    def contextMenuServer(self, point):
+        item = self.uic.folderServer.currentItem()
+        menu = QMenu(self.uic.folderServer)
+        download = menu.addAction("Download")
+        download.setEnabled(False)
+        edit = menu.addAction("View/ Edit")
+        menu.addSeparator()
+        mkdir = menu.addAction("Create directory")
+        newfile = menu.addAction("Create new file")
+        refresh = menu.addAction("Refresh")
+        menu.addSeparator()
+        remove = menu.addAction("Delete")
+        rename = menu.addAction("Rename")
+        
+        action = menu.exec_(self.uic.folderServer.mapToGlobal(point))
+        if action == refresh:
+            self.testPrint()
+
+    
     # Tạo hàm để test các nút hoặc là sự kiện 1 cách nhanh chóng
     def testPrint(self):
         print("Test Ok")
@@ -141,16 +234,22 @@ class MainWindow():
         self.uic.inputDirPathServer.setText("Not connected to any server")
         self.uic.loadDirPathServer.setEnabled(False)
     def enabledComponents(self):
-        pathServer = self.ftp.pwd()
-        self.uic.inputDirPathServer.setText(pathServer)
+        self.pathServer = self.ftp.pwd()
+        self.uic.inputDirPathServer.setText(self.pathServer)
         self.uic.inputDirPathServer.setReadOnly(False)
         self.uic.loadDirPathServer.setEnabled(True)
+        # Khi đã kết nối thì cho phép click right
+        self.uic.folderServer.customContextMenuRequested.connect(self.contextMenuServer)
+        
+        self.downloadToRemoteFileList()
     # Hàm load dir Path để hiển thị folder cho nười dùng
     def loadDirPathServer(self):
         dirPathServer = self.uic.inputDirPathServer.text()
         try:
             self.ftp.cwd(dirPathServer)
-            print(self.ftp.dir())
+            self.uic.folderServer.clear()
+            self.downloadToRemoteFileList()
+            # print(self.ftp.dir())
         except:
             content = "Can't find path named " + dirPathServer
             self.printError(content) 
@@ -161,12 +260,8 @@ class MainWindow():
             if n < 1024:
                 return f"{n:.2f}{unit}{suffix}"
             n /= 1024
-
-    def get_datetime_format(date_time):
-        # Chuyển đổi thành đối tượng DateTime
-        date_time = datetime.strptime(date_time, "%Y%m%d%H%M%S")
-        # Chuyển đổi thành chuỗi thời gian ngày có thể đọc được của con người
-        return date_time.strftime("%Y/%m/%d %H:%M:%S")
+            # return f"{n:.2f}{unit}{suffix}"
+    
     # Xóa folder, file
     def deleteDataServer(self, dataName):
         self.ftp.delete(dataName)
@@ -208,16 +303,11 @@ class MainWindow():
     def open_url(self, url):
         # Mở link trang web
         webbrowser.open(url)
-    def contextMenuEvent(self, event):
-        contextMenu = QMenu(self.uic.folderServer)
-        creatDirectory = contextMenu.addAction("New Folder")
-        download = contextMenu.addAction("Download")
-        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
     # Hàm load dir Path để hiển thị folder cho nười dùng
     def loadDirPathLocal(self):
-        dirPathLocal = self.uic.inputDirPathLocal.text()
-        self.uic.model.setRootPath(r""+dirPathLocal)
-        self.uic.folderLocal.setRootIndex(self.uic.model.index(dirPathLocal))  
+        self.pathLocal = self.uic.inputDirPathLocal.text()
+        self.uic.model.setRootPath(r""+self.pathLocal)
+        self.uic.folderLocal.setRootIndex(self.uic.model.index(self.pathLocal))  
     # Sử dụng để hiển thị trạng thái cho người dùng
     # Sử dụng để hiển thị lỗi với nội dung màu đỏ
     def printError(self, message):
